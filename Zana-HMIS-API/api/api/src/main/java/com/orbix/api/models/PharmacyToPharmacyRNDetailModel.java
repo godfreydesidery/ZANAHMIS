e239db481af25b434fa5d13ef8006211c39d23e1
godfreydesidery <desideryg@gmail.com>
2024-01-12 21:47:58 +0300

    Regular update

--- Zana-HMIS-API/api/api/src/main/java/com/orbix/api/models/PharmacyToPharmacyRNDetailModel.java
+++ Zana-HMIS-API/api/api/src/main/java/com/orbix/api/models/PharmacyToPharmacyRNDetailModel.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 import com.orbix.api.domain.Item;
 import com.orbix.api.domain.Medicine;
+//import com.orbix.api.domain.PharmacyToPharmacyBatch;
+import com.orbix.api.domain.PharmacyToPharmacyRN;
 import com.orbix.api.domain.StoreToPharmacyBatch;
 import com.orbix.api.domain.StoreToPharmacyRN;
-import com.orbix.api.domain.StoreToPharmacyTO;
 
 import lombok.Data;
 
@@ -18,22 +19,19 @@ import lombok.Data;
  *
  */
 @Data
-public class StoreToPharmacyRNDetailModel {
+public class PharmacyToPharmacyRNDetailModel {
 	public Long id = null;
 	
-	public double orderedPharmacySKUQty = 0;
-	public double receivedPharmacySKUQty = 0;
-	public double receivedStoreSKUQty = 0;
+	public double orderedQty = 0;
+	public double receivedQty = 0;
 		
 	public String status = "";
 		
-    public StoreToPharmacyRN storeToPharmacyRN = null;
+    public PharmacyToPharmacyRN pharmacyToPharmacyRN = null;
 	
     public Medicine medicine = null;
 	
-    public Item item = null;
-    
     public String created;
     
-    public List<StoreToPharmacyBatch> storeToPharmacyBatches;
+    //public List<PharmacyToPharmacyBatch> pharmacyToPharmacyBatches;
 }
